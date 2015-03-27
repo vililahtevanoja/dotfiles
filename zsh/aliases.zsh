2c61b57bda03b761b88e711d5a2e85bc9f1d7c6e
Vili Lähtevänoja <vili.lahtevanoja@gmail.com>
2015-03-27 13:52:58 +0200

    Updated .zshrc-file and aliases.

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,1 +1,5 @@
-alias reload!='. ~/.zshrc'
+alias lakkairc="ssh lakka -t 'screen -rd; bash -l'"
+alias reload=". ~/.zshrc && echo 'ZSH config reloaded from ~/.zshrc'"
+alias zshrc="vim ~/.zshrc && reload"
+alias vimrc="vim ~/.vimrc"
+alias diffs="diff --side-by-side --suppress-common-lines"
